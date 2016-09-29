1c95cb24b1bd0528f181e22ebcdccd1fe6413d6a
Lasse Haverinen <lassehav@oamk.fi>
2016-09-29 11:49:52 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+var cloudinary = require('cloudinary');
+var cloudinaryStorage = require('multer-storage-cloudinary');
+var express = require('express');
+var multer = require('multer');
+
+var app = express();
+
+// Config cloudinary storage for multer-storage-cloudinary
+var storage = cloudinaryStorage({
+  cloudinary: cloudinary,
+  folder: 'someklone',
+  allowedFormats: ['jpg', 'png'],
+});
+
+var parser = multer({ storage: storage });
+        
+// You can store key-value pairs in express, here we store the port setting
+app.set('port', (process.env.PORT || 80));
+
+// Simple hello world route
+app.get('/', function(req, res) {
+    res.send("Hello world");
+});
+
+app.post('/upload', parser.single('image'), function (req, res) {       
+    console.log(req.file);
+    res.sendStatus(201);
+});
+
+// start listening for incoming HTTP connections
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+});
